91ef315ae4db1a9eb31fe1f38d7762031894af3d
liangxing1116@gmail.com <liangxing@xiaomi.com>
2023-03-14 07:44:06 +0800

    update: async

--- js/daxiwenhua.js
+++ js/daxiwenhua.js
@@ -5,15 +5,15 @@ const hasImg = {
     '00997_1_F': '6a63f6246b600c333e2b2437ef6b9d04d8f9a1b7.jpeg'
 }
 
-async function request(url) {
-    const res =  await fetch(url)
-    return res.text()
+function request(url) {
+    const res = fetch(url).then(res => res.text())
+    return res
 }
 
-let text = await request('/json/' + file + '.wav.txt')
-
-if (hasImg[file]) {
-    text = '<img src="/imgs/' + hasImg[file]+'" />' + text
-}
-
-document.querySelector('.content').innerHTML = text
+request('/json/' + file + '.wav.txt').then(text => {
+    if (hasImg[file]) {
+        text = '<img src="/imgs/' + hasImg[file]+'" />' + text
+    }
+    
+    document.querySelector('.content').innerHTML = text
+})
